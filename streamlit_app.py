ab1925acb7f07cf809c74ffdf54f33fb92c30b67
AlcibiadesTophetScipio <42887529+AlcibiadesTophetScipio@users.noreply.github.com>
2021-03-02 16:44:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,62 @@ import math
 import pandas as pd
 import streamlit as st
 
+from pathlib import Path
+import os
+
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Check and make dir.
 """
 
+input_dir = st.text_input('Source Dir:', './')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if st.button('Enter'):
+    source_dir = Path(input_dir)
+    if source_dir.is_dir():
+        check_dir_list = os.listdir(source_dir)
+        
+        curr_dir_info = {'dir':[], 'file':[]}
+        for file in check_dir_list:
+            file_item = source_dir / file
+            if file_item.is_file():
+                curr_dir_info['file'] += str(file_item)
+            elif file_item.is_dir():
+                curr_dir_info['dir'] += str(file_item)
+        st.write(curr_dir_info)
+    else:
+        st.text('Please verify the dir:{}'.format(input_dir))
+else:
+    pass
+        
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# """
+# # Welcome to Streamlit!
 
-    points_per_turn = total_points / num_turns
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# forums](https://discuss.streamlit.io).
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# In the meantime, below is an example of what you can do with just a few lines of code:
+# """
+
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+#     Point = namedtuple('Point', 'x y')
+#     data = []
+
+#     points_per_turn = total_points / num_turns
+
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
